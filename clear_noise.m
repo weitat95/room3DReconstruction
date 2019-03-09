47c51d489715ec2f3eea120838779cc986b59e95
Wei Tat Lee <weitat95@live.com>
2019-03-09 22:32:29 +0000

    change flying pixel

--- clear_noise.m
+++ clear_noise.m
@@ -49,15 +49,33 @@ end
 
 %% Task 3, Removing Flying Pixel
 % Hyperparameter
-num_neighbors = 10; % number of neighbors to be inlier (not flying pixels)
-std_dev_thresh = 0.5; % Standard deviation to be considered inliers
-
+K = 100;
+DIST_THRESH = 0.02;
+INLIER_THRESH = 10;
 color_pc_3 = ori_color;
 xyz_pc_3 = ori_loc;
 
-[~, indx_xyz_yes, indx_xyz_no] = pcdenoise(pc,'NumNeighbors',num_neighbors, 'Threshold', std_dev_thresh);
+indx_xyz_no = [];
+for i = 1 : length(xyz_pc_3)
+    i
+    if (isnan(xyz_pc_3(i,1)))
+       continue; 
+    end
+    [ ~, nearest_dist ] =findNearestNeighbors(pc, xyz_pc_3(i,:), K); 
+    if (sum(nearest_dist<DIST_THRESH) < INLIER_THRESH)
+        indx_xyz_no = [indx_xyz_no ; i];
+    end
+end
+% num_neighbors = 2; % number of neighbors to be inlier (not flying pixels)
+% std_dev_thresh = 1.5; % Standard deviation to be considered inliers
+% 
+% color_pc_3 = ori_color;
+% xyz_pc_3 = ori_loc;
+% 
+% [~, indx_xyz_yes, indx_xyz_no] = pcdenoise(pc,'NumNeighbors',num_neighbors, 'Threshold', std_dev_thresh);
+
 color_pc_3(indx_xyz_no,:) = 0;
-new_pc_3 = pointCloud(xyz_pc_3, 'Color' , color_pc_3);
+%new_pc_3 = pointCloud(xyz_pc_3, 'Color' , color_pc_3);
 
 bin_3 = color_pc_3~=0;
 t3_mask_bin = reshape(bin_3(:,1), [640, 480]);
